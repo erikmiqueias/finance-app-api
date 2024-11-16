ab20fe78994fb2393559ac8cbbca4cef075fe331
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-11-16 17:10:57 -0400

    feat: add GetTransactionsByUserIdController

---
+++ src/controllers/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,39 @@
+import { UserNotFoundError } from '../../errors/user.js';
+import { ok } from '../helpers/http.js';
+import { checkIfIdIsValid } from '../helpers/user.js';
+import {
+    invalidIdResponse,
+    requiredFieldIsMissingResponse,
+} from '../helpers/validation.js';
+
+export class GetTransactionByUserId {
+    constructor(getTransactionsByUserIdUseCase) {
+        this.getTransactionsByUserIdUseCase = getTransactionsByUserIdUseCase;
+    }
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.query.userId;
+
+            if (!userId) {
+                return requiredFieldIsMissingResponse('userId');
+            }
+
+            const userIdIsValid = checkIfIdIsValid(userId);
+
+            if (!userIdIsValid) {
+                return invalidIdResponse();
+            }
+
+            const transactions =
+                await this.getTransactionsByUserIdUseCase.execute({
+                    userId,
+                });
+
+            return ok(transactions);
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                throw new UserNotFoundError(params.userId);
+            }
+        }
+    }
+}
