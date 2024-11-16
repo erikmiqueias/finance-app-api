a03f49a3015480c417ee848efd258e4f125ab0b3
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-11-16 18:21:00 -0400

    feat: add UpdateTransactionController

---
+++ src/controllers/transaction/update-transaction.js
@@ -0,0 +1,66 @@
+import {
+    badRequest,
+    checkIfAmountIsValid,
+    checkIfIdIsValid,
+    checkIfTypeIsValid,
+    invalidAmountResponse,
+    invalidIdResponse,
+    invalidTypeReponse,
+    ok,
+    serverError,
+} from '../helpers/index.js';
+
+export class UpdateTransactionController {
+    constructor(updateTransactionUseCase) {
+        this.updateTransactionUseCase = updateTransactionUseCase;
+    }
+    async execute(httpRequest) {
+        try {
+            const idIsValid = checkIfIdIsValid(httpRequest.params.id);
+
+            if (!idIsValid) {
+                return invalidIdResponse();
+            }
+
+            const params = httpRequest.body;
+
+            const allowedFields = ['name', 'date', 'amount', 'type'];
+
+            const someFieldIsNotAllowed = Object.keys(params).some((field) => {
+                return !allowedFields.includes(field);
+            });
+
+            if (someFieldIsNotAllowed) {
+                return badRequest({
+                    message: 'Some provided field is not allowed',
+                });
+            }
+
+            if (params.amount) {
+                const amountIsValid = checkIfAmountIsValid(params.amount);
+
+                if (!amountIsValid) {
+                    return invalidAmountResponse();
+                }
+            }
+
+            if (params.type) {
+                const typeIsValid = checkIfTypeIsValid(params.type);
+
+                if (!typeIsValid) {
+                    return invalidTypeReponse();
+                }
+            }
+
+            const transaction = await this.updateTransactionUseCase.execute(
+                httpRequest.params.id,
+                params,
+            );
+
+            return ok(transaction);
+        } catch (error) {
+            console.error(error);
+            return serverError();
+        }
+    }
+}
