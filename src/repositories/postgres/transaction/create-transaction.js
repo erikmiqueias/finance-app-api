274311af5dc8d02aa34d0c5c57dcd0eee5cecbd5
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-11-23 16:29:03 -0400

    feat: ensure update and GetTransactionByUserId repositories use Prisma

--- src/repositories/postgres/transaction/create-transaction.js
+++ src/repositories/postgres/transaction/create-transaction.js
@@ -2,7 +2,6 @@ import { prisma } from '../../../../prisma/prisma.js';
 
 export class PostgresCreateTransactionRepository {
     async execute(createTransactionParams) {
-        console.log(createTransactionParams);
         return await prisma.transaction.create({
             data: createTransactionParams,
         });
