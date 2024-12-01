519e0da35ed403e8276729d9b8639ab78377d87c
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-12-01 18:06:04 -0400

    chore: add initial jest setup

---
+++ jest.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+    collectCoverage: true,
+
+    coverageDirectory: 'coverage',
+
+    coverageProvider: 'v8',
+};
+
+export default config;
