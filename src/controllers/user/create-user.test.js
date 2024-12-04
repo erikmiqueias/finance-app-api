bf1365050ec74dbd90c5984bc45a4a0e75198772
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-12-04 13:21:12 -0400

    test: ensure 400 is returned if password is less than 6 chars

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -131,4 +131,24 @@ describe('Create User Controller', () => {
 
         expect(result.statusCode).toBe(400);
     });
+
+    it('Should return 400 if password is less than 6 characters', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+
+        const httpRequest = {
+            body: {
+                first_name: 'John',
+                last_name: 'Doe',
+                email: 'john',
+                password: '123',
+            },
+        };
+
+        const result = await createUserController.execute(httpRequest);
+
+        expect(result.statusCode).toBe(400);
+    });
 });
