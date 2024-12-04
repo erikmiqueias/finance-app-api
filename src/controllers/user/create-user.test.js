cb5db835d018439935915332566a6a573ed68de3
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-12-04 13:18:24 -0400

    test: ensure 400 is returned if password  is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -112,4 +112,23 @@ describe('Create User Controller', () => {
 
         expect(result.statusCode).toBe(400);
     });
+
+    it('Should return 400 if password is not provided', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+
+        const httpRequest = {
+            body: {
+                first_name: 'John',
+                last_name: 'Doe',
+                email: 'john',
+            },
+        };
+
+        const result = await createUserController.execute(httpRequest);
+
+        expect(result.statusCode).toBe(400);
+    });
 });
