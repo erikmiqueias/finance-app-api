3beddef5eadc663795302490caeef5cc8fefe322
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-12-04 13:14:55 -0400

    test: ensure 400 is returned if email is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -73,4 +73,23 @@ describe('Create User Controller', () => {
 
         expect(result.statusCode).toBe(400);
     });
+
+    it('Should return 400 if email is not provided', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+
+        const httpRequest = {
+            body: {
+                first_name: 'John',
+                last_name: 'Doe',
+                password: '1234567',
+            },
+        };
+
+        const result = await createUserController.execute(httpRequest);
+
+        expect(result.statusCode).toBe(400);
+    });
 });
