0c8fb01a114cb95b0484ab5027a898112dd0d11d
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-12-27 22:53:26 -0400

    test: ensure GetUserBalanceController returns 400 if userId is invalid

---
+++ src/controllers/user/get-user-balance.test.js
@@ -0,0 +1,42 @@
+import { faker } from '@faker-js/faker';
+import { GetUserBalanceController } from './get-user-balance';
+
+describe('GetUserBalanceController', () => {
+    class GetUserBalanceUseCaseStub {
+        async execute() {
+            return faker.number.int();
+        }
+    }
+
+    const makeSut = () => {
+        const getUserBalanceUseCase = new GetUserBalanceUseCaseStub();
+        const sut = new GetUserBalanceController(getUserBalanceUseCase);
+
+        return { sut, getUserBalanceUseCase };
+    };
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+    };
+
+    it('Should return 200 when getting user balance', async () => {
+        // arrange
+        const { sut } = makeSut();
+        // act
+
+        const httpResponse = await sut.execute(httpRequest);
+        // assert
+        expect(httpResponse.statusCode).toBe(200);
+    });
+
+    it('Should return 400 when userId is invalid', async () => {
+        // arrange
+        const { sut } = makeSut();
+        // act
+        const result = await sut.execute({ params: { userId: 'invalid_id' } });
+        // assert
+        expect(result.statusCode).toBe(400);
+    });
+});
