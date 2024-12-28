19b80ccc88057c4b12d9430f3937383a9e348f77
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-12-27 23:45:35 -0400

    test: ensure UpdateUserController returns 400 if an unallowed field is provided

--- src/controllers/user/update-user-.test.js
+++ src/controllers/user/update-user-.test.js
@@ -83,4 +83,19 @@ describe('UpdateUserController', () => {
         // assert
         expect(result.statusCode).toBe(400);
     });
+
+    it('Should return 400 when an unallowed field is provided', async () => {
+        // arrange
+        const { sut } = makeSut();
+        // act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: {
+                ...httpRequest.body,
+                unallowed_field: faker.lorem.word(),
+            },
+        });
+        // assert
+        expect(result.statusCode).toBe(400);
+    });
 });
